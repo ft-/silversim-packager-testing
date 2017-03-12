d1a48ecd7ad7b0a68866453f5df1ff3260459443
ft <ft@noemail>
2017-03-12 15:57:02 +0100

    added basics for SilverSim.Packager

---
+++ SilverSim/Packager/Application.cs
@@ -0,0 +1,22 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Updater;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Threading;
+
+namespace SilverSim.Packager
+{
+    static class Application
+    {
+        [SuppressMessage("Gendarme.Rules.Exceptions", "DoNotSwallowErrorsCatchingNonSpecificExceptionsRule")]
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "SilverSim:Packager";
+            CoreUpdater.Instance.CheckForUpdates();
+            CoreUpdater.Instance.VerifyInstallation();
+        }
+    }
+}
